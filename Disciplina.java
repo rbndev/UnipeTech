d2727617ab1cd49ee2441a4f436de320993feab3
Rbn DEV <62131778+rbnndev@users.noreply.github.com>
2021-04-05 15:32:45 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,21 @@
+public class Disciplina {
+	private String materias;
+	private int nota;
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	public String getMaterias() {
+		return materias;
+	}
+
+	public void setMaterias(String materias) {
+		this.materias = materias;
+	}
+	
+}
